946aa19363c2e0cb0bb139197bac180ebfc4db05
quocnguyen <quocnv@9Pay.vn>
2025-04-08 10:15:50 +0700

    change base64 json file

--- services/firebase_service.js
+++ services/firebase_service.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-const serviceAccount = require('../services/shoes-701d0-firebase-adminsdk-0bsy8-2267792de4.json');
+
+const base64 = process.env.FIREBASE_CREDENTIALS_BASE64;
+const serviceAccount = JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: 'gs://shoes-701d0.appspot.com',
